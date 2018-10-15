06523c2fb0c0c4dd1014f42578d321be7de3d56b
Carlos Andres Cadena Padilla <carlos.cadena@correounivalle.edu.co>
2018-10-15 10:36:21 -0500

    Primer Commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express()
+const hbs = require('hbs');
+require('./hbs/helpers.js')
+ 
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//expres HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    
+    
+    res.render('home',{
+        nombre: 'Carlos Andres',
+       
+    });
+
+});
+
+app.get('/about', (req, res) => {
+    
+    
+    res.render('about',{
+        
+    });
+
+});
+
+
+
+
+app.listen(port, () =>{
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
